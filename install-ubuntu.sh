b64448db12046a498e5c39d330f67509d6655a8d
JUB0T <jforeverything2007@gmail.com>
2022-06-17 13:44:41 +0200

    ...

---
+++ install-ubuntu.sh
@@ -0,0 +1,33 @@
+apt update
+apt install nginx
+
+killall nginx
+
+npm install forever -g
+
+cd /root
+rm -r Dynamo
+rm -r dynamo
+
+git clone https://github.com/jareer12/dashboard.git Dynamo
+
+mkdir /var/www/dynamofront
+mkdir /root/dynamo
+
+mv /root/Dynamo/client/* /var/www/dynamofront
+mv /root/Dynamo/server/* /root/dynamo
+
+cd /var/www/dynamofront
+npm install
+npm run build
+
+cd ..
+mv ./dynamofront/dist ./dynamo
+rm -r dynamofront
+
+cd /root
+cd ./dynamo
+
+npm install
+echo "PUBLIC_PORT=7000" > .env
+forever start ./src/Index.js
