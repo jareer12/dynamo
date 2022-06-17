afc369ff30397a6f62f441dfec2905d3be046df5
JUB0T <jforeverything2007@gmail.com>
2022-06-17 11:57:02 +0200

    ...

--- scripts/delete_all_images.sh
+++ scripts/delete_all_images.sh
@@ -1,3 +1,3 @@
-docker volume rm $(docker volume ls -q)
-docker rmi -f $(docker images -aq)
-docker rm $(docker ps -a -q)
+docker volume rm $(docker volume ls -q) --force
+docker rm $(docker ps -a -q) --force
+docker rmi -f $(docker images -aq) --force
