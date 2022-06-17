39989c27e3481df63611d9a5ac66383b44ccc1c5
JUB0T <jforeverything2007@gmail.com>
2022-06-17 14:01:17 +0200

    ...

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -11,7 +11,7 @@ rm -r dynamo
 rm -r /root/dynamo
 rm -r /var/www/dynamofront
 
-git clone https://github.com/jareer12/dashboard.git Dynamo
+git clone https://github.com/jareer12/dynamo.git Dynamo
 
 mkdir /var/www/dynamofront
 mkdir /root/dynamo
@@ -34,9 +34,39 @@ npm install
 echo "PUBLIC_PORT=7000" > .env
 forever start ./src/Index.js
 
+rm /etc/nginx/sites-enabled/default.save
 NGINX_CONF=/etc/nginx/sites-enabled/default
 
 rm $NGINX_CONF
 touch $NGINX_CONF
-content=$(wget https://raw.githubusercontent.com/jareer12/dashboard/main/nginx/ubuntu.conf -q -O -)
+content='
+server {
+    listen 80;
+    server_name api.jubot.site;
+
+    location / {
+        proxy_pass http://127.0.0.1:7000;
+    }
+}
+
+server {
+    listen      80;
+    server_name dash.jubot.site;
+
+    root     /var/www/dynamo;
+    index   index.html index.htm;
+
+    location / {
+        root /var/www/dynamo;
+        try_files $uri /index.html;
+    }
+        
+    error_log  /var/log/nginx/vue-app-error.log;
+    access_log /var/log/nginx/vue-app-access.log;
+}
+
+'
 echo $content >  $NGINX_CONF
+
+killall nginx
+nginx
