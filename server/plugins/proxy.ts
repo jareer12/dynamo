c3d7b8fc1da02bcd77140654a558634c0bea8df8
JUB0T <jforeverything2007@gmail.com>
2022-06-18 22:44:10 +0200

    Re-install js

--- server/plugins/proxy.ts
+++ server/plugins/proxy.ts
@@ -1,4 +1,3 @@
-// import DB from "../plugins/sql";
 import { spawn } from "child_process";
 import { readFileSync, writeFileSync } from "fs";
 
@@ -36,15 +35,7 @@ server {
     } catch (err) {
       console.log(err);
     }
-    // try {
-    //   DB.getReverseProxies(50)
-    //     .then((data) => {
-    //       console.log(data);
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //     });
-    // } catch {}
+
     spawn("nginx");
   } catch (err) {
     console.log(err);
