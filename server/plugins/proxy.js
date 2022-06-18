b86bae720893e6732467c6a9b2955edfef83ab92
JUB0T <jforeverything2007@gmail.com>
2022-06-18 22:00:27 +0200

    ...

--- server/plugins/proxy.js
+++ server/plugins/proxy.js
@@ -3,9 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const OS = process.platform;
 if (OS.toLowerCase() === "linux") {
-    const NginxConfPath = `/etc/nginx/sites-eanbled/default`;
-    const NginxConf = (0, fs_1.readFileSync)(NginxConfPath, "utf-8");
-    console.log(NginxConf);
+    try {
+        const NginxConfPath = `/etc/nginx/sites-enabled/default`;
+        const NginxConf = (0, fs_1.readFileSync)(NginxConfPath, "utf-8");
+        console.log(NginxConf);
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 else {
     console.log(`Operating System(OS) is not Ubuntu, Detected ${OS}, Proxy will not work.`);
--- server/plugins/proxy.ts
+++ server/plugins/proxy.ts
@@ -2,9 +2,13 @@ import { readFileSync } from "fs";
 
 const OS: string = process.platform;
 if (OS.toLowerCase() === "linux") {
-  const NginxConfPath: string = `/etc/nginx/sites-eanbled/default`;
-  const NginxConf: String = readFileSync(NginxConfPath, "utf-8");
-  console.log(NginxConf);
+  try {
+    const NginxConfPath: string = `/etc/nginx/sites-enabled/default`;
+    const NginxConf: String = readFileSync(NginxConfPath, "utf-8");
+    console.log(NginxConf);
+  } catch (err) {
+    console.log(err);
+  }
 } else {
   console.log(
     `Operating System(OS) is not Ubuntu, Detected ${OS}, Proxy will not work.`
